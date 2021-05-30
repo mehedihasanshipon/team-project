2707eb6cbe4159428fcc68ce1937659cce47475d
nurulabsar-git <mohammad.nurulabsar20203025@yahoo.com>
2021-05-30 20:26:06 +0600

    update

--- src/components/Home/Home/House/SubHouseDetails.js
+++ src/components/Home/Home/House/SubHouseDetails.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import { Container } from 'react-bootstrap';
 import { useParams } from 'react-router';
 import fakehouse from '../FakeData';
 
@@ -11,12 +12,22 @@ const SubHouseDetails = () => {
         
     // },[])
     return (
-        <div className="pt-5 mt-5">
-          
-            <h1>This is service details page</h1>
-            <h4>{certainProduct.id}</h4>
-            <h5>{certainProduct.apartmentInfo}</h5>
-        </div>
+        <Container>
+            <div className="header container" style={{marginBottom: '20px'}}>
+                <div>
+                    <img src={certainProduct.img} alt="" style={{width: '60%', borderRadius: '5px'}} className="flui" />
+                </div>
+                
+                <div>
+                <h5 style={{textAlign: 'left'}}>Apartment Info.</h5>
+                <p style={{textAlign: 'justify'}}>{certainProduct.apartmentInfo}</p>
+                <h5 style={{textAlign:'left'}}>Price Details</h5>
+                <p style={{textAlign: 'justify'}}>{certainProduct.priceDetails}</p>
+                <h6 style={{textAlign: 'left'}}>Property Details</h6>
+                <p style={{textAlign: 'justify'}}>{certainProduct.propertyDetails}</p>
+                </div>
+            </div>
+        </Container>
     );
 };
 
