f3db838e589a93e912513ca4a239e4175cd249a3
nurulabsar-git <mohammad.nurulabsar20203025@yahoo.com>
2021-05-30 19:47:47 +0600

    update

--- src/components/Home/Home/FakeData.js
+++ src/components/Home/Home/FakeData.js
@@ -31,7 +31,19 @@ import myimg3 from "../../../image/Rectangle 398.png"
           location: "Nasirabad H/S, Chattogram",
           bedroom: 3,
           bathroom: 2,
-          price: 500
+          price: 500,
+          apartmentInfo:`3000 sq-ft., 3 Bedroom, Semi-furnished, Luxurious, South facing Apartment for Rent in Rangs Malancha, Melbourne.`,
+          priceDetails:`Rent/Month: $550 (negotiable)
+          Service Charge : 8,000/= Tk per month, subject to change
+          Security Deposit : 3 month’s rent
+          Flat Release Policy : 3 months earlier notice required`,
+          propertyDetails:`Address & Area : Rangs Malancha, House-68, Road-6A (Dead End Road), Dhanmondi Residential Area.
+          Flat Size : 3000 Sq Feet.
+          Floor :  A5 (5th Floor) (6 storied Building ) (South Facing Unit)
+          Room Category : 3 Large Bed Rooms with 3 Verandas, Spacious Drawing, Dining & Family Living Room, Highly Decorated Kitchen with Store Room and Servant room with attached Toilet.
+          Facilities : 1 Modern Lift, All Modern Amenities & Semi Furnished.
+          Additional Facilities : a. Electricity with full generator load, b. Central Gas Geyser, c. 2 Car Parking with 1 Driver’s Accommodation, d. Community Conference Hall, e. Roof Top Beautified Garden and Grassy Ground, f. Cloth Hanging facility with CC camera
+          `
        },
        
        {  id: 3,
@@ -40,7 +52,19 @@ import myimg3 from "../../../image/Rectangle 398.png"
           location: "Nasirabad H/S, Chattogram",
           bedroom: 3,
           bathroom: 2,
-          price: 500
+          price: 500,
+          apartmentInfo:`3000 sq-ft., 3 Bedroom, Semi-furnished, Luxurious, South facing Apartment for Rent in Rangs Malancha, Melbourne.`,
+          priceDetails:`Rent/Month: $550 (negotiable)
+          Service Charge : 8,000/= Tk per month, subject to change
+          Security Deposit : 3 month’s rent
+          Flat Release Policy : 3 months earlier notice required`,
+          propertyDetails:`Address & Area : Rangs Malancha, House-68, Road-6A (Dead End Road), Dhanmondi Residential Area.
+          Flat Size : 3000 Sq Feet.
+          Floor :  A5 (5th Floor) (6 storied Building ) (South Facing Unit)
+          Room Category : 3 Large Bed Rooms with 3 Verandas, Spacious Drawing, Dining & Family Living Room, Highly Decorated Kitchen with Store Room and Servant room with attached Toilet.
+          Facilities : 1 Modern Lift, All Modern Amenities & Semi Furnished.
+          Additional Facilities : a. Electricity with full generator load, b. Central Gas Geyser, c. 2 Car Parking with 1 Driver’s Accommodation, d. Community Conference Hall, e. Roof Top Beautified Garden and Grassy Ground, f. Cloth Hanging facility with CC camera
+          `
        },
        {  id: 4,
           img: myimg,
@@ -48,7 +72,19 @@ import myimg3 from "../../../image/Rectangle 398.png"
           location: "Nasirabad H/S, Chattogram",
           bedroom: 3,
           bathroom: 2,
-          price: 500
+          price: 500,
+          apartmentInfo:`3000 sq-ft., 3 Bedroom, Semi-furnished, Luxurious, South facing Apartment for Rent in Rangs Malancha, Melbourne.`,
+          priceDetails:`Rent/Month: $550 (negotiable)
+          Service Charge : 8,000/= Tk per month, subject to change
+          Security Deposit : 3 month’s rent
+          Flat Release Policy : 3 months earlier notice required`,
+          propertyDetails:`Address & Area : Rangs Malancha, House-68, Road-6A (Dead End Road), Dhanmondi Residential Area.
+          Flat Size : 3000 Sq Feet.
+          Floor :  A5 (5th Floor) (6 storied Building ) (South Facing Unit)
+          Room Category : 3 Large Bed Rooms with 3 Verandas, Spacious Drawing, Dining & Family Living Room, Highly Decorated Kitchen with Store Room and Servant room with attached Toilet.
+          Facilities : 1 Modern Lift, All Modern Amenities & Semi Furnished.
+          Additional Facilities : a. Electricity with full generator load, b. Central Gas Geyser, c. 2 Car Parking with 1 Driver’s Accommodation, d. Community Conference Hall, e. Roof Top Beautified Garden and Grassy Ground, f. Cloth Hanging facility with CC camera
+          `
        },
        
        {  id: 5,
@@ -57,7 +93,19 @@ import myimg3 from "../../../image/Rectangle 398.png"
           location: "Nasirabad H/S, Chattogram",
           bedroom: 3,
           bathroom: 2,
-          price: 500
+          price: 500,
+          apartmentInfo:`3000 sq-ft., 3 Bedroom, Semi-furnished, Luxurious, South facing Apartment for Rent in Rangs Malancha, Melbourne.`,
+          priceDetails:`Rent/Month: $550 (negotiable)
+          Service Charge : 8,000/= Tk per month, subject to change
+          Security Deposit : 3 month’s rent
+          Flat Release Policy : 3 months earlier notice required`,
+          propertyDetails:`Address & Area : Rangs Malancha, House-68, Road-6A (Dead End Road), Dhanmondi Residential Area.
+          Flat Size : 3000 Sq Feet.
+          Floor :  A5 (5th Floor) (6 storied Building ) (South Facing Unit)
+          Room Category : 3 Large Bed Rooms with 3 Verandas, Spacious Drawing, Dining & Family Living Room, Highly Decorated Kitchen with Store Room and Servant room with attached Toilet.
+          Facilities : 1 Modern Lift, All Modern Amenities & Semi Furnished.
+          Additional Facilities : a. Electricity with full generator load, b. Central Gas Geyser, c. 2 Car Parking with 1 Driver’s Accommodation, d. Community Conference Hall, e. Roof Top Beautified Garden and Grassy Ground, f. Cloth Hanging facility with CC camera
+          `
        },
        
        {  id: 6,
@@ -66,7 +114,19 @@ import myimg3 from "../../../image/Rectangle 398.png"
           location: "Nasirabad H/S, Chattogram",
           bedroom: 3,
           bathroom: 2,
-          price: 500
+          price: 500,
+          apartmentInfo:`3000 sq-ft., 3 Bedroom, Semi-furnished, Luxurious, South facing Apartment for Rent in Rangs Malancha, Melbourne.`,
+          priceDetails:`Rent/Month: $550 (negotiable)
+          Service Charge : 8,000/= Tk per month, subject to change
+          Security Deposit : 3 month’s rent
+          Flat Release Policy : 3 months earlier notice required`,
+          propertyDetails:`Address & Area : Rangs Malancha, House-68, Road-6A (Dead End Road), Dhanmondi Residential Area.
+          Flat Size : 3000 Sq Feet.
+          Floor :  A5 (5th Floor) (6 storied Building ) (South Facing Unit)
+          Room Category : 3 Large Bed Rooms with 3 Verandas, Spacious Drawing, Dining & Family Living Room, Highly Decorated Kitchen with Store Room and Servant room with attached Toilet.
+          Facilities : 1 Modern Lift, All Modern Amenities & Semi Furnished.
+          Additional Facilities : a. Electricity with full generator load, b. Central Gas Geyser, c. 2 Car Parking with 1 Driver’s Accommodation, d. Community Conference Hall, e. Roof Top Beautified Garden and Grassy Ground, f. Cloth Hanging facility with CC camera
+          `
        },
        
     
--- src/components/Home/Home/House/SubHouseDetails.js
+++ src/components/Home/Home/House/SubHouseDetails.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useParams } from 'react-router';
+import fakehouse from '../FakeData';
 
 const SubHouseDetails = () => {
     const {cardId} = useParams();
-    // console.log("useParams", cardId)
-    
+    console.log("useParams", cardId)
+    const certainProduct = fakehouse?.find(datum => datum.id == cardId)
+    console.log(certainProduct);
+    // useEffect(()=> {
+        
+    // },[])
     return (
         <div className="pt-5 mt-5">
+          
             <h1>This is service details page</h1>
+            <h4>{certainProduct.id}</h4>
+            <h5>{certainProduct.apartmentInfo}</h5>
         </div>
     );
 };
