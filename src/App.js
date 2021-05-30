b22fbc4121f29a9eab1167f7ac83d0026d4d037e
mehedidevms <shipon.mistrisolutions@gmail.com>
2021-05-30 18:24:58 +0600

    Login page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import Home from './components/Home/Home/Home';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Login from './components/Login/Login';
 
 function App() {
   return (
-    <div >
-      <Home />
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+           <Home />
+        </Route>
+        
+        <Route>
+          <Login />
+        </Route>
+      </Switch>
+     
+    </Router>
   );
 }
 
