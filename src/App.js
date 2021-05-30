35ff330f4cff2aa499a65f7cbf10ad772cfde589
nurulabsar-git <mohammad.nurulabsar20203025@yahoo.com>
2021-05-30 19:11:46 +0600

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import Home from './components/Home/Home/Home';
-<<<<<<< HEAD
 import {
   BrowserRouter as Router,
   Switch,
@@ -7,8 +6,6 @@ import {
   Link
 } from "react-router-dom";
 import SubHouseDetails from './components/Home/Home/House/SubHouseDetails';
-
-
 function App() {
   return (
     <div>
@@ -24,25 +21,6 @@ function App() {
     </Router>
 
     </div>
-=======
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import Login from './components/Login/Login';
-
-function App() {
-  return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-           <Home />
-        </Route>
-        
-        <Route>
-          <Login />
-        </Route>
-      </Switch>
-     
-    </Router>
->>>>>>> b22fbc4121f29a9eab1167f7ac83d0026d4d037e
   );
 }
 
